6493365418acc125b6d2f553cf34f517c9da02a3
ahmed zakaria <48223187+ahmedzak1@users.noreply.github.com>
2019-03-12 03:57:42 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1x0ZBAZhk6hYM7CdVeEDalt0dIspNz3-V'
+export_file_name = 'cuisine.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['middel eastren food' , 'italian food' , 'asian food', 'french food']
 path = Path(__file__).parent
 
 app = Starlette()
